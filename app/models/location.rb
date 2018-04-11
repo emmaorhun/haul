9b166eb2f4df6da2ef16bdf695097db0e9dc9c45
Emma Orhun <emmaorhun@shopify.com>
2018-04-11 19:42:25 -0400

    location tests

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,6 +1,10 @@
 class Location < ApplicationRecord
 
   validates_format_of :postal_code,:with => /[ABCEGHJKLMNPRSTVXY][0-9][ABCEGHJKLMNPRSTVWXYZ] ?[0-9][ABCEGHJKLMNPRSTVWXYZ][0-9]/, :on => :create
+  validates :street_address, presence: true
+  validates :province, presence: true
+  validates :city, presence: true
+  
   belongs_to :mall
 
 end
--- test/models/location_test.rb
+++ test/models/location_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class LocationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  bayshore = Mall.create(name: "Bayshore Shopping Center")
+
+  test 'location validity' do
+    location = Location.create(mall: bayshore, street_address: "100 Bayshore Dr", province: "Ontario", city: "Ottawa", postal_code: "K2H7C8")
+    assert location.save
+  end
+
+  test 'mall presence' do
+    location = Location.create(street_address: "100 Bayshore Dr", province: "Ontario", city: "Ottawa", postal_code: "K2H7C8")
+    assert_not location.save
+  end
+
+  test 'street_address presence' do
+    location = Location.create(mall: bayshore, province: "Ontario", city: "Ottawa", postal_code: "K2H7C8")
+    assert_not location.save
+  end
+
+  test 'province presence' do
+    location = Location.create(mall: bayshore, street_address: "100 Bayshore Dr", city: "Ottawa", postal_code: "K2H7C8")
+    assert_not location.save
+  end
+
+  test 'city presence' do
+    location = Location.create(mall: bayshore, street_address: "100 Bayshore Dr", province: "Ontario", postal_code: "K2H7C8")
+    assert_not location.save
+  end
+
+  test 'postal_code presence' do
+    location = Location.create(mall: bayshore, street_address: "100 Bayshore Dr", province: "Ontario", city: "Ottawa")
+    assert_not location.save
+  end
+
+  test 'postal_code regex' do
+    location = Location.create(mall: bayshore, street_address: "100 Bayshore Dr", province: "Ontario", city: "Ottawa", postal_code: "beepboop")
+    assert_not location.save
+  end
+
 end
