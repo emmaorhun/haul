b8502f3a53736fb9811dae56155fcb55f0eaf124
Emma Orhun <emmaorhun@shopify.com>
2018-04-11 19:13:40 -0400

    product test

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  bayshore = Mall.create(name: "Bayshore Shopping Center")
+  old_navy = Store.create(mall: bayshore, name: "Old Navy")
+
+  test 'product validity' do
+    product = Product.create(store: old_navy, stock: 200)
+    assert product.save
+  end
+
+  test 'store presence' do
+    product = Product.create(stock: 200)
+    assert_not product.save
+  end
+
+  test 'stock presence' do
+    product = Product.create(store: old_navy)
+    assert_not product.save
+  end
+
 end
