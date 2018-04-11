28778ce26d903944d4649c20d63ee8ff05e45fc2
Emma Orhun <emmaorhun@shopify.com>
2018-04-11 18:22:52 -0400

    user tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,9 +2,19 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  test 'valid user' do
-      user = User.create(username: 'elizabeth', password: 'harmonica', email: "beep@boop.com")
-      assert user.valid?
+  test 'vemail regex' do
+      user = User.create(username: 'elizabeth', password: 'harmonica', email: "beepboop.com")
+      assert_not user.valid?
+  end
+
+  test 'password length' do
+    user = User.create(username: 'elizabeth', password: ':)', email: "beep@boop.com")
+    assert_not user.valid?
+  end
+
+  test 'password presence' do
+    user = User.create(username: 'elizabeth', password: '', email: "beep@boop.com")
+    assert_not user.valid?
   end
 
 
