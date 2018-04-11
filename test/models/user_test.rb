95728cc7136356ca1501a88a2053da17d23ffdbe
Emma Orhun <emmaorhun@shopify.com>
2018-04-11 19:07:49 -0400

    user and variable tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,19 +2,40 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
 
-  test 'vemail regex' do
+  test 'user validity' do
+    user = User.create(username: 'elizabeth', password: 'harmonica', email: "beep@boop.com")
+    assert user.save
+  end
+
+  test 'email presence' do
+    user = User.create(username: 'elizabeth', password: 'harmonica')
+    assert_not user.save
+  end
+
+  test 'email regex' do
       user = User.create(username: 'elizabeth', password: 'harmonica', email: "beepboop.com")
-      assert_not user.valid?
+      assert_not user.save
+  end
+
+  test 'password presence' do
+    user = User.create(username: 'elizabeth', email: "beep@boop.com")
+    assert_not user.save
   end
 
   test 'password length' do
     user = User.create(username: 'elizabeth', password: ':)', email: "beep@boop.com")
-    assert_not user.valid?
+    assert_not user.save
   end
 
-  test 'password presence' do
-    user = User.create(username: 'elizabeth', password: '', email: "beep@boop.com")
-    assert_not user.valid?
+  test 'username presence' do
+    user = User.create(password: 'harmonica', email: "beep@boop.com")
+    assert_not user.save
+  end
+
+  test 'username uniqueness' do
+    user1 = User.create(username: "tobias", password: 'harmonica', email: "beep@boop.com")
+    user2 = User.create(username: "tobias", password: 'yeet', email: "ham@sandwich.com")
+    assert_not user2.save
   end
 
 
--- test/models/variable_test.rb
+++ test/models/variable_test.rb
@@ -1,7 +1,35 @@
 require 'test_helper'
 
 class VariableTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  bayshore = Mall.create(name: "Bayshore Shopping Center")
+  old_navy = Store.create(mall: bayshore, name: "Old Navy")
+  on_tshirt = Product.create(store: old_navy, stock: 200)
+
+  test 'variable validity' do
+    variable = Variable.new(store: old_navy, product: on_tshirt, type_of_variable: 'color', option: 'green')
+    assert variable.save
+  end
+
+  test 'store presence' do
+    variable = Variable.new(product: on_tshirt, type_of_variable: 'color', option: 'green')
+    assert_not variable.save
+  end
+
+
+  test 'product presence' do
+    variable = Variable.new(store: old_navy, option: 'green')
+    assert_not variable.save
+  end
+
+  test 'type_of_variable presence' do
+    variable = Variable.new(store: old_navy, product: on_tshirt, option: 'green')
+    assert_not variable.save
+  end
+
+  test 'option presence' do
+    variable = Variable.new(store: old_navy, product: on_tshirt, type_of_variable: 'color')
+    assert_not variable.save
+  end
+
 end
